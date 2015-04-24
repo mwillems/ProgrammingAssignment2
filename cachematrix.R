458f9733fc248a42ce28f25753195de092fccc07
Matthieu Willems <matthieu_willems@yahoo.fr>
2015-04-24 21:52:55 +0200

    Complete functions
    Add a primary code to get result expected

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,73 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  inv <- NULL
+  
+##set
+set <- function(y) {
+  x <<- y
+  inv <<- NULL
+} 
 
+##get
+get <- function() {x}
+
+##setinv
+setinv <- function (inver) inv<<- inver
+
+##getinv
+getinv <- function() inv
+
+list (get=get, set=set, setinv=setinv, getinv= getinv)
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  
+  # return inverted matrix from cache if it exists
+  # else create the matrix in working environment
+  if (!is.null(inv)) {
+    message("getting cached data")
+    
+    # display matrix in console
+    return(inv)
+  }
+  
+  else {
+  # create matrix since it does not exist
+  matrix <- x$get()
+  
+  # make sure matrix is square and invertible
+  # if not, handle exception cleanly
+ # tryCatch( {
+    # set and return inverse of matrix
+    inv <- solve(matrix, ...)
+#  },
+# error = function(e) {
+#   message("Error:")
+#   message(e)
+    
+#   return(NA)
+
+# },
+# warning = function(e) {
+#   message("Warning:")
+#   message(e)
+    
+#   return(NA)
+# },
+#  finally = {
+    # set inverted matrix in cache
+    x$setinv(inv)
 }
+# } )
+  
+  # display matrix in console
+  return (inv)
+}
+
+##MWI le 21/04/2015
